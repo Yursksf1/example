fdbc50db1db3db455401c5017807fd83f531dca2
Yurley Sánchez <yurs.ksf1@gmail.com>
2020-11-07 09:15:22 -0500

    add game

--- juego.py
+++ juego.py
@@ -1,12 +1,10 @@
 import random
+res = int(input('En que numero estoy pensando?'))
+print('Numero pensado', res, "\n\n")
 
-print(random.randrange(10))
-
-
-'''
-1) guardar el valor en una variable
-2) hacer un while que pregunte que numero estoy pensando?
-2.1) hacer un print que diga  ue pregunte que numero estoy pensando?
-2.2) imput para ingresar un numero
-3) si es igual imprimar que adivinaste se acabo el jueggo
-'''
+while True:
+    ran = random.randint(1, 10)
+    print('random', ran)
+    if ran == res:
+        break
+print("Felicitaciones..Ganaste...se ha adivinado el numero ", res)
